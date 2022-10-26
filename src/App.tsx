a5ccb27467fb6271b025b799ebbb5b4ec2dfbff8
Alexander.D <lonegdd.druid@gmail.com>
2022-10-26 22:45:12 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">asdfasdvzxcv
+      asdfzxcv
+      asdf
+      zxcv
+      asdfa
+      sdf
+    </div>
+  );
+}
+
+export default App;
